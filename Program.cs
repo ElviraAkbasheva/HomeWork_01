20f8ce905d473dc2854b17824831989a3129dc3f
ElviraAkbasheva <165509920+ElviraAkbasheva@users.noreply.github.com>
2024-12-12 22:32:39 +0300

    Added commands: /addtask, /showtasks, /removetask

--- Program.cs
+++ Program.cs
@@ -1,29 +1,40 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using System.Threading.Channels;
+using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Xml.Linq;
 
 
 namespace HomeWork_1
 {
     internal class User
     {
-        private string _userName;
-
+        public string UserName { get; set; }
+        private List<string> _tasks = new List<string>();
         public User(string defaultUserName)
         {
-            _userName = defaultUserName;
-        }
-        public void Set(string userName)
-        {
-            _userName = userName;
+            UserName = defaultUserName;
         }
 
-        public string GetName()
+        public void AddTask(string task)
         {
-            return _userName;
+            _tasks.Add(task);
+        }
+
+        public void RemoveTask(int number)
+        {
+            _tasks.RemoveAt(number);
+        }
+
+        public List<string> GetTasksList()
+        {
+            return _tasks;
+        }
+        public int GetTasksLenght()
+        {
+            return _tasks.Count;
         }
     }
-
 
     internal class Program
     {
@@ -46,17 +57,10 @@ namespace HomeWork_1
             {
                 Print("Введи своё имя", 1);
                 string name = Console.ReadLine();
-                int charCounter = 0;
-                foreach (char c in name)
+
+                if (CheckInput(name))
                 {
-                    if (c != ' ')
-                    {
-                        charCounter++;
-                    }
-                }
-                if (charCounter > 0)
-                {
-                    user.Set(name);
+                    user.UserName = name;
                     Print($"Приятно познакомиться, {name}", 1);
                     Print("Теперь Тебе доступна команда \"/echo\"");
                     break;
@@ -70,16 +74,19 @@ namespace HomeWork_1
 
         public static void Help(User user)
         {
-            Print($"Я помогу тебе {user.GetName()}", 1);
+            Print($"Я помогу тебе {user.UserName}", 1);
             Print("/start - эта команда позволит нам познакомиться, и я смогу называть Тебя по имени");
             Print("/info  - эта команда позволит Тебе узнать обо мне больше");
             Print("/echo  - эта команда введенная с аргументом, например /echo Hello, возвращает введенный текст, в данном примере \"Hello\"");
+            Print("/addtask - эта команда позволит Тебе создать задание для меня");
+            Print("/showtasks - эта команда позволит Тебе посмотреть список заданий для меня");
+            Print("/removetask - эта команда удаляет задание из списка");
             Print("/exit  - этой командой мы попрощаемся и программа закроется");
         }
 
         public static void Info(User user)
         {
-            Print($"{user.GetName()}, я расскажу Тебе о себе", 1);
+            Print($"{user.UserName}, я расскажу Тебе о себе", 1);
             Print("Я - программа имитации интерактивного бота \"Хоттабыч 1.0\", моя дата рождения 30.11.2024 г.");
         }
         public static void Echo(User user, string word)
@@ -87,7 +94,7 @@ namespace HomeWork_1
             if (word.Length >= 6)
             {
                 word = word.Remove(0, 6);
-                Print($"{user.GetName()}, Твои слова эхом отразились в моём сердце и возвращаются к Тебе! {word}", 1);
+                Print($"{user.UserName}, Твои слова эхом отразились в моём сердце и возвращаются к Тебе! {word}", 1);
             }
             else
             {
@@ -100,6 +107,96 @@ namespace HomeWork_1
             Environment.Exit(0);
         }
 
+        public static void AddTask(User user)
+        {
+            while (true)
+            {
+                Print($"{user.UserName}, я жду Твоих указаний! Напиши, что Ты хочешь.");
+                string task = Console.ReadLine();
+
+                if (CheckInput(task))
+                {
+                    user.AddTask(task);
+                    Print("Я запомнил Твоё указание!");
+                    break;
+                }
+                else
+                {
+                    Print("Ты так и не дал указание!");
+                }
+            }
+        }
+
+        public static void ShowTasks(User user)
+        {
+            int taskLenght = user.GetTasksLenght();
+            if (taskLenght == 0)
+            {
+                Print($"{user.UserName}, список заданий пуст");
+            }
+            else
+            {
+                Print($"{user.UserName}, я помню Твои указания!");
+                int number = 1;
+                foreach (string task in user.GetTasksList())
+                {
+                    Print($"{number}. {task}");
+                    number++;
+                }
+            }
+
+        }
+
+        public static void RemoveTask(User user)
+        {
+            ShowTasks(user);
+            int taskLenght = user.GetTasksLenght();
+            if (taskLenght == 0)
+            {
+                Print("Так что удалять нечего.");
+            }
+            else
+            {
+                while (true)
+                {
+                    Print("Какое указание Ты хочешь удалить? Введи его номер.");
+                    string input = Console.ReadLine();
+                    bool result = int.TryParse(input, out int taskNum);
+
+                    if (taskNum > 0 && taskNum <= taskLenght)
+                    {
+                        user.RemoveTask(taskNum - 1);
+                        Print($"Я удалил задание номер {taskNum}!");
+                        break;
+                    }
+                    else
+                    {
+                        Print("Ты даешь неверный номер задания или это вообще не число! Попробуй еще раз.");
+                    }
+                }
+            }
+        }
+
+        public static bool CheckInput(string input)
+        {
+            int charCounter = 0;
+            foreach (char c in input)
+            {
+                if (c != ' ')
+                {
+                    charCounter++;
+                }
+            }
+            if (charCounter > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         static void Main()
         {
             string defaultUserName = "Незнакомец";
@@ -110,6 +207,9 @@ namespace HomeWork_1
             Print("/start");
             Print("/help");
             Print("/info");
+            Print("/addtask");
+            Print("/showtasks");
+            Print("/removetask");
             Print("/exit");
 
             while (true)
@@ -130,7 +230,7 @@ namespace HomeWork_1
                 }
                 else if (command.Contains("/echo"))
                 {
-                    if (user.GetName() == defaultUserName)
+                    if (user.UserName == defaultUserName)
                     {
                         Console.WriteLine($"Для выполнения команды \"/echo\", представься, {defaultUserName}, через команду \"/start\"");
                     }
@@ -143,11 +243,23 @@ namespace HomeWork_1
                 {
                     Exit();
                 }
+                else if (command.Contains("/addtask"))
+                {
+                    AddTask(user);
+                }
+                else if (command.Contains("/showtasks"))
+                {
+                    ShowTasks(user);
+                }
+                else if (command.Contains("/removetask"))
+                {
+                    RemoveTask(user);
+                }
                 else
                 {
                     Print("Такой команды не существует!", 1);
                 }
-             }
+            }
         }
     }
 }
